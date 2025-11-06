2feee5cbcaf505fd35c564f380d5d4c22b8e27c8
AltSystem42 <50642697+AltSystem42@users.noreply.github.com>
2025-11-06 10:06:52 -0400

    finished exercise 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let result = str
+    for(let i = 0; i < (num - 1); i++){
+        result = result.concat(str)
+    }
+    if (num === 0){
+        return ""
+    } else if(num <= -1) {
+        return "ERROR"
+    } else {
+        return result
+    }
+    
 };
 
 // Do not edit below this line
