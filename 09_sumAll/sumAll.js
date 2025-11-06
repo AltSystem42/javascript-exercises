34d21ac8260c74c456def97339d37a326bb5e34a
AltSystem42 <50642697+AltSystem42@users.noreply.github.com>
2025-11-06 14:20:36 -0400

    finished exercise 09_sumall

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    // take in two numbers and add the all the number between x and y into the total
+    if( x < 0 || y < 0 || x % 1 !== 0 || y % 1 !== 0 || typeof(x) === "string" || typeof(y) === "string") {
+        return "ERROR"
+    } else{
+        if(x < y){
+            let result = x
+            for(let i = x; i <= y; i++){
+                result = result + i
+            }
+            return result - x
+        } else {
+            let result = y
+            for(let i = y; i <= x; i++){
+                result = result + i
+            }
+            return result - y
+        }
+    }
 };
 
 // Do not edit below this line
