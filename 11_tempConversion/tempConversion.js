f75316cdcdb5dd121de281bdda4713df4a152a9b
AltSystem42 <50642697+AltSystem42@users.noreply.github.com>
2025-11-06 14:58:27 -0400

    finished exercise 11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  let result = (x - 32) * (5/9)
+  result = parseFloat(result.toFixed(1));
+  return result
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(x) {
+  let result = (x * (9/5) + 32)
+  result = parseFloat(result.toFixed(1));
+  return result
 };
 
 // Do not edit below this line
