344e18698cefcd7ed4010c5d0e7bfacf02f751a3
AltSystem42 <50642697+AltSystem42@users.noreply.github.com>
2025-11-09 19:41:49 -0400

    finished exercise 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const oldest = people.sort((a, b) =>{
+        let year = new Date()
+        if(!a.yearOfDeath)
+        {
+            a.yearOfDeath = year.getFullYear()
+        }
+        if(!b.yearOfDeath){
+            b.yearOfDeath = year.getFullYear()
+        }
+        const lastGuy = a.yearOfDeath - a.yearOfBirth
+        const nextGuy = b.yearOfDeath - b.yearOfBirth
+        return lastGuy > nextGuy ? -1 : 1
+    })
+    return oldest[0];
 };
 
 // Do not edit below this line
