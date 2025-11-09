38c3b791aa9e27a55d6ab5bae0c8689dc55b4660
AltSystem42 <50642697+AltSystem42@users.noreply.github.com>
2025-11-09 18:43:05 -0400

    finished exercise 15_getTheTitles

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    //loop over each array object and grab the title of the object
+    let list = []
+    list = books.map((book) => {
+        return book.title
+    })
+    return list
 };
 
 // Do not edit below this line
