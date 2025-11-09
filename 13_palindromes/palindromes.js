9923ba2344029dbc7ebf5f8722893bdd6de1c062
AltSystem42 <50642697+AltSystem42@users.noreply.github.com>
2025-11-09 17:21:12 -0400

    finished exercise 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,4 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.toLowerCase().replaceAll(/[^a-z0-9]/g, "");
+    let array = string.split("")
+    let newArray = array.slice().reverse()
+    let counter = 0
+    for(let i = 0; i < array.length; i++){
+        if(array[i] === newArray[i]){
+            counter++}
+    }
+    if(counter === array.length){
+        return true
+    } else {
+        return false
+    }
 
 };
 
