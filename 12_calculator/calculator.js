a09ccbea06078241e226fa258a75ad7a6fe726c0
AltSystem42 <50642697+AltSystem42@users.noreply.github.com>
2025-11-09 16:22:57 -0400

    finished exercise 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	const newArr = arr.reduce((total, number)=>{
+    
+    return total + number;
+  }, 0)
+  return newArr;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  const newArr = arr.reduce((total, number) => {
+    return total * number
+  }, 1) 
+  return newArr;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let c = 1
+  for(let i = 0; i < b; i++){
+    c *= a
+  }
+	return c
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if(a === 0){
+    return 1} else {
+  let c = a * factorial((a - 1))
+  return c}
 };
 
 // Do not edit below this line
